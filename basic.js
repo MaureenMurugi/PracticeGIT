1b5ae9888e913a2a410ddedf513345813f76b8bc
MaureenMurugi <maureenmurugi07@gmail.com>
2024-04-30 21:42:27 +0300

    My first Javascript code

---
+++ basic.js
@@ -0,0 +1,41 @@
+// Variables
+
+// DATA TYPES
+// Number 0 - 9
+
+//Maths operations [+, -, *, /, **, %]
+// [BODMAS] - BIDMAS
+//shorthand operations
+
+//Strings
+
+
+//Arrays - collection of things
+
+// let myStory = 'The quick brown fox jumped over the lazy dog';
+// let author = 'Maureen Murugi';
+// let age = 24;
+
+// //Template string array - ``${}
+// blog = `The winning blog of the year is ${myStory} which is written by ${author} at the age of ${age}`;
+
+// console.log(blog);
+
+//Boolean
+let age = 24;
+console.log(age != 25); 
+// ==true != false - loose comparison
+// ===true !== false - strict comparison
+
+/*  
+    true   true   true
+    true   false  false
+    false  true   false
+    false  false   false
+*/
+
+let num1 = 10;
+let num2 = 12;
+
+comp = num1 <= num2 ;
+console.log(comp);
---
+++ javascript.md
@@ -0,0 +1,9 @@
+1. Variables are data structures that store values
+   - let - updates the variable
+   - const - doesn't change the variable
+- Naming convention - camelCase
+- Variable naming does not start with numbers
+- Reserved keywords do not create the names
+- Make the name meaningful
+
+Methods - fxns associated with a particular data type
